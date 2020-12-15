943fbdd00cd4a9b1fb1f4ed3ed814012cb88adc7
Bartosz Zarzecki <bart@parachut.co>
2020-12-15 15:21:26 +0100

    forgot password and update function (auth), admin functionality for the users

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,10 @@ const {
 	register,
 	login,
 	getMe,
+	forgotPassword,
+	resetPassword,
+	updateDetails,
+	updatePassword,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
@@ -16,5 +20,21 @@ router.route('/register').post(register);
 router.route('/login').post(login);
 
 router.route('/me').get(protect, getMe);
+
+router
+	.route('/forgotpassword')
+	.post(forgotPassword);
+
+router
+	.route('/resetpassword/:resettoken')
+	.put(resetPassword);
+
+router
+	.route('/updatedetails')
+	.put(protect, updateDetails);
+
+router
+	.route('/updatepassword')
+	.put(protect, updatePassword);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const bootcamps = require('./routes/bootcamps');
 const courses = require('./routes/courses');
 const auth = require('./routes/auth');
 const connectDB = require('./config/db');
-
+const users = require('./routes/users');
 //load env
 dotenv.config({
 	path: './config/config.env',
@@ -40,6 +40,7 @@ app.use(
 app.use('/api/v1/auth', auth);
 app.use('/api/v1/bootcamps', bootcamps);
 app.use('/api/v1/courses', courses);
+app.use('/api/v1/users', users);
 
 app.use(errorHandler);
 
